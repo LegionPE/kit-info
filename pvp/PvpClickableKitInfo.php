8bdf304ee0b11afa5127e20d449db1718b9c6d0c
PEMapModder <pemapmodder1970@gmail.com>
2015-09-01 15:32:42 +0800

    Added more details

--- pvp/PvpClickableKitInfo.php
+++ pvp/PvpClickableKitInfo.php
@@ -18,6 +18,7 @@ namespace libtheta\info\pvp;
 use legionpe\theta\utils\MUtils;
 
 class PvpClickableKitInfo extends PvpKitInfo{
+	const BASE_PRICE = 1;
 	/** @var int */
 	private $typeId;
 	public function __construct($type, $amplitude, $count){
@@ -25,12 +26,42 @@ class PvpClickableKitInfo extends PvpKitInfo{
 		$this->typeId = $type;
 	}
 	public function getHourPrice(){
-		return $this->getBaseHourPrice() * (1 + $this->getAmplitude()) * $this->getCount();
+		return $this->getBaseHourPrice() * (1 + $this->getAmplitude()) * $this->getCount() * self::BASE_PRICE;
 	}
 	public function getBaseHourPrice(){
 		switch($this->typeId){
 			case self::SPECIAL_KNOCKBACK_BOMB:
-				return 50;
+				return 10;
+			case self::SPECIAL_SPEED:
+				return 8;
+			case self::SPECIAL_SLOW_BOMB:
+				return 10;
+			case self::SPECIAL_NAUSEA_BOMB:
+				return 5;
+			case self::SPECIAL_JUMP:
+				return 8;
+			case self::SPECIAL_INVISIBILITY:
+				return 12;
+			case self::SPECIAL_STRENGTH:
+				return 10;
+			case self::SPECIAL_WEAKNESS_BOMB:
+				return 15;
+			case self::SPECIAL_RESISTANCE:
+				return 10;
+			case self::SPECIAL_VULNERABILITY_BOMB:
+				return 15;
+			case self::SPECIAL_OXYGEN:
+				return 8;
+			case self::SPECIAL_FIRE_RESISTANCE:
+				return 10;
+			case self::SPECIAL_REGENERATION:
+				return 15;
+			case self::SPECIAL_POISON_BOMB:
+				return 15;
+			case self::SPECIAL_WITHER_BOMB:
+				return 20;
+			case self::SPECIAL_DIARRHEA_BOMB:
+				return 10;
 		}
 		return 0;
 	}
@@ -43,6 +74,34 @@ class PvpClickableKitInfo extends PvpKitInfo{
 				return "Knockback bomb";
 			case self::SPECIAL_SPEED:
 				return "Speed";
+			case self::SPECIAL_SLOW_BOMB:
+				return "Slow bomb";
+			case self::SPECIAL_NAUSEA_BOMB:
+				return "Nausea bomb";
+			case self::SPECIAL_JUMP:
+				return "Jump";
+			case self::SPECIAL_INVISIBILITY:
+				return "Invisibility";
+			case self::SPECIAL_STRENGTH:
+				return "Strength";
+			case self::SPECIAL_WEAKNESS_BOMB:
+				return "Weakness bomb";
+			case self::SPECIAL_RESISTANCE:
+				return "Resistance";
+			case self::SPECIAL_VULNERABILITY_BOMB:
+				return "Vulnerability bomb";
+			case self::SPECIAL_OXYGEN:
+				return "Oxygen";
+			case self::SPECIAL_FIRE_RESISTANCE:
+				return "Fire resistance";
+			case self::SPECIAL_REGENERATION:
+				return "Regeneration";
+			case self::SPECIAL_POISON_BOMB:
+				return "Poison bomb";
+			case self::SPECIAL_WITHER_BOMB:
+				return "Wither bomb";
+			case self::SPECIAL_DIARRHEA_BOMB:
+				return "Diarrhea bomb";
 		}
 		return "unknown";
 	}
@@ -97,10 +156,31 @@ class PvpClickableKitInfo extends PvpKitInfo{
 			case self::SPECIAL_SLOW_BOMB:
 				return 100 + $this->getAmplitude() * 20;
 			case self::SPECIAL_NAUSEA_BOMB:
-				return 150 + $this->getAmplitude() * 20;
+				return 150 + $this->getAmplitude() * 30;
 			case self::SPECIAL_JUMP:
 				return 100 + $this->getAmplitude() * 20;
 			case self::SPECIAL_INVISIBILITY:
+				return 100 + $this->getAmplitude() * 50;
+			case self::SPECIAL_STRENGTH:
+				return 150 + $this->getAmplitude() * 50;
+			case self::SPECIAL_WEAKNESS_BOMB:
+				return 100 + $this->getAmplitude() * 20;
+			case self::SPECIAL_RESISTANCE:
+				return 100 + $this->getAmplitude() * 50;
+			case self::SPECIAL_VULNERABILITY_BOMB:
+				return 100 + $this->getAmplitude() * 25;
+			case self::SPECIAL_OXYGEN:
+				return 100 + $this->getAmplitude() * 25;
+			case self::SPECIAL_FIRE_RESISTANCE:
+				return 100 + $this->getAmplitude() * 25;
+			case self::SPECIAL_REGENERATION:
+				return 60 + $this->getAmplitude() * 20;
+			case self::SPECIAL_POISON_BOMB:
+				return 60 + $this->getAmplitude() * 20;
+			case self::SPECIAL_WITHER_BOMB:
+				return 60 + $this->getAmplitude() * 20;
+			case self::SPECIAL_DIARRHEA_BOMB:
+				return 200 + $this->getAmplitude() * 100;
 		}
 		return 0;
 	}
